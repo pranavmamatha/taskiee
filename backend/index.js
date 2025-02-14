4a777667f2d3be64af5e7d069eae2267b68915b4
pranavranjithkannan <pranavranjithkannan@gmail.com>
2025-02-14 10:53:53 +0530

    signup option

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(express.json());
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/todo", todoRoute);
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const dotenv = require("dotenv");
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const { User } = require("../db/db");
+
+const signupSchema = zod.object({
+  username: zod.string().min(3),
+  firstName: zod.string().min(1).max(30),
+  lastName: zod.string().min(1).max(30),
+  password: zod.string().min(6),
+});
+
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const { success } = signupSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(409).json({
+      message: "User already exist",
+    });
+  }
+
+  const user = await User.create(req.body);
+  const userId = user._id;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.status(201).json({
+    message: "User Created Successfully",
+    token: token,
+  });
+});
 
 module.exports = router;
