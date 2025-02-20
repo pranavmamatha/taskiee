6364afc90c5e5bd9b0366ac0546472c73ef4c005
pranavranjithkannan <pranavranjithkannan@gmail.com>
2025-02-21 05:01:16 +0530

    fixing build errors

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Heading from "../components/Heading";
-import SubHeading from "../components/Subheading";
+import SubHeading from "../components/SubHeading";
 import InputBox from "../components/InputBox";
-import Button from "../components/button";
+import Button from "../components/Buttonutton";
 import BottomWarning from "../components/BottomWarning";
 import Warning from "../components/Warning";
 import { useState, useEffect } from "react";
